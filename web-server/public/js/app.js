cc907b4d587f57807f96423c804ef97440347a7a
Arntit Zonia <arditzonia@gmail.com>
2023-04-01 16:08:59 +0100

    set up location-form submit handler

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,54 @@
-console.log("Script running!");
+const getWeatherData = async (address) => {
+  try {
+    const { data } = await axios.get(
+      `http://localhost:3000/weather?address=${address}`
+    );
+
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.error({ error });
+
+    return error;
+  }
+};
+
+const renderValueToDom = (elmSelector, value) => {
+  const elm = document.querySelector(`${elmSelector}`);
+
+  elm.textContent = value;
+};
+
+const handleFormSubmit = () => {
+  const locationForm = document.querySelector(".location-form");
+  const search = document.querySelector(".location-form input");
+  const loadingElm = document.querySelector(".loading");
+
+  locationForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if (!search.value) return;
+
+    renderValueToDom(".error-message", "");
+    
+    loadingElm.textContent = "Loading...";
+
+    const {
+      forecast,
+      location: locationData,
+      address,
+      error,
+    } = await getWeatherData(search.value);
+
+    loadingElm.textContent = "";
+    
+    if (error) renderValueToDom(".error-message", error);
+    
+    renderValueToDom(".forecast", forecast);
+    renderValueToDom(".location", locationData?.location);
+    renderValueToDom(".address", address);
+  });
+};
+
+handleFormSubmit();
